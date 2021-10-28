439244361cb546da44cc23accb88f0e7f02fed79
thekokoloko <sotoramiro30@yahoo.com>
2021-10-28 12:05:49 -0700

    first commit

---
+++ TinDog 12.04.03 PM/TinDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinDogApp.swift
+//  TinDog
+//
+//  Created by Ramiro Soto on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TinDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
