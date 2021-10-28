f7bc39827f3099f66a29575d053204001048abbb
thekokoloko <sotoramiro30@yahoo.com>
2021-10-28 14:26:45 -0700

    first commit

---
+++ TinDog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinDog
+//
+//  Created by Ramiro Soto on 10/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!Ramiro")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
